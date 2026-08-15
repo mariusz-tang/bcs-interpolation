ba45ee14a497615e5f0890ab37d5d5efa9b86c06
Mariusz Tang <dev@mariusztang.com>
2026-08-15 11:29:01 +0100

    fix(docs): typo in help message

--- src/bcsi/cli.py
+++ src/bcsi/cli.py
@@ -266,7 +266,7 @@ def get_parser() -> argparse.ArgumentParser:
         "--num-frames",
         type=int,
         default=15,
-        help="number of frames at which to evaluate the ARAP metric (default: 10)",
+        help="number of frames at which to evaluate the ARAP metric (default: 15)",
     )
     deformation_energy.set_defaults(outfile=True, func_name="deformation_energy")
 
